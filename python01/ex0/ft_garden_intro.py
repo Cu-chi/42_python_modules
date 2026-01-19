21233f77861967636b04550f7a0033f8dc29ccb6
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-01-19 19:29:30 +0100

    dev(module/1): WIP

---
+++ python01/ex0/ft_garden_intro.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    name: str = "Rose"
+    height: str = "25"
+    age: str = "30"
+    print("=== Welcome to My Garden ===")
+    print("Plant: " + name)
+    print("Height: " + height + "cm")
+    print("Age: " + age + " days")
+    print("=== End of Program ===")
---
+++ python01/ex1/ft_garden_data.py
@@ -0,0 +1,33 @@
+class Plant:
+    """
+    A class representing a plant
+
+    Attributes:
+        name (str): Plant name
+        height (str): Plant height
+        age (str): Plant age
+    """
+    def __init__(self, name: str, height: str, age: str):
+        """
+        Initialize a Plant object
+
+        Parameters:
+            name (str): Plant name
+            height (str): Plant height
+            age (str): Plant age
+        """
+        self.name = name
+        self.height = height
+        self.age = age
+
+
+if __name__ == "__main__":
+    plants = [
+        Plant("Rose", "25", "30"),
+        Plant("Sunflower", "80", "45"),
+        Plant("Cactus", "15", "120")
+    ]
+    ("=== Garden Plant Registry ===")
+    for i in range(3):
+        print(f"{plants[i].name}: {plants[i].height}cm, {plants[i].age} days "
+              "old")
