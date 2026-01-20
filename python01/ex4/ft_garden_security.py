2dba331661a66386613fcb77e70fd0bbe2e0d637
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-01-20 13:20:24 +0100

    dev(module1/ex4): ex4

---
+++ python01/ex4/ft_garden_security.py
@@ -0,0 +1,70 @@
+class SecurePlant:
+    """
+    A secure class representing a plant
+    """
+    def __init__(self, name: str, height: int, age: int):
+        """
+        Initialize a SecurePlant object
+
+        Parameters:
+            name (str): Plant name
+            height (int): Plant height
+            age (int): Plant age
+        """
+        print("Plant created: " + name)
+        self.__name = name
+        self.set_height(height)
+        self.set_age(age)
+
+    def set_height(self, height: int):
+        """
+        Setter for height
+        """
+        if height < 0:
+            print(f"Invalid operation attempted: height {height}cm "
+                  "[REJECTED]")
+            print("Security: Negative height rejected")
+            return
+        self.__height = height
+        print(f"Height updated: {height}cm [OK]")
+
+    def set_age(self, age: int):
+        """
+        Setter for age
+        """
+        if age < 0:
+            print(f"Invalid operation attempted: age {age} days "
+                  "[REJECTED]")
+            print("Security: Negative age rejected")
+            return
+        self.__age = age
+        print(f"Age updated: {age} days [OK]")
+
+    def get_height(self):
+        """
+        Getter for height
+        """
+        return self.__height
+
+    def get_age(self):
+        """
+        Getter for age
+        """
+        return self.__age
+
+    def get_info(self):
+        """
+        Print info of the plant
+        """
+        print(f"Current plant: {self.__name} ({self.__height}cm, {self.__age} "
+              "days)")
+
+
+if __name__ == "__main__":
+    print("=== Garden Security System ===")
+    rose = SecurePlant("Rose", 25, 30)
+    rose.set_height(-10)
+    rose.set_age(-5)
+    rose.set_height(26)
+    rose.set_age(31)
+    rose.get_info()
