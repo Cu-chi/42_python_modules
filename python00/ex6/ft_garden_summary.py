52af6e06a4ba03bfa06eafae974b8eac9ce4ec82
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-01-28 16:38:21 +0100

    fix(module0/ex6): typo

--- python00/ex6/ft_garden_summary.py
+++ python00/ex6/ft_garden_summary.py
@@ -1,6 +1,6 @@
 def ft_garden_summary() -> None:
     garden_name: str = input("Enter garden name: ")
     number_of_plants: str = input("Enter number of plants: ")
-    print(f"Plants: {garden_name}")
+    print(f"Garden: {garden_name}")
     print(f"Plants: {number_of_plants}")
     print("Status: Growing well!")
