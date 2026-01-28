67972db84fb0a876af9b9d78c71b0c788699e7a2
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-01-28 15:47:20 +0100

    fix(module0/ex7): print seed type even if unknown type unit

--- python00/ex7/ft_seed_inventory.py
+++ python00/ex7/ft_seed_inventory.py
@@ -7,4 +7,4 @@ def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
         print(f"{seed_type.capitalize()} seeds: covers {quantity} square "
               "meters")
     else:
-        print("Unknown unit type")
+        print(f"{seed_type.capitalize()} seeds: Unknown unit type")
