a5d6c559ffc00cb0d06182aaef350f697ad46c9e
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-02-07 15:45:50 +0100

    tweak(module04/ex3): added a way to check if file are closed

--- python04/ex3/ft_vault_security.py
+++ python04/ex3/ft_vault_security.py
@@ -1,26 +1,26 @@
 def main() -> None:
     print("=== CYBER ARCHIVES - VAULT SECURITY SYSTEM ===\n")
     print("Initiating secure vault access...")
+    classified = None
+    protocols = None
     try:
-        with open("classified_data.txt", "r") as file:
+        with open("classified_data.txt", "r") as classified:
             print("\nSECURE EXTRACTION:")
-            print(file.read())
-    except FileNotFoundError as e:
-        print(f"Error classified data: '{e.filename}' not found")
-    except Exception as e:
-        print(f"Error: {e}")
-    try:
-        with open("security_protocols.txt", "a") as file:
-            log: str = "[CLASSIFIED] New security protocols archived"
+            print(classified.read())
+        with open("security_protocols.txt", "a") as protocols:
+            log: str = "[CLASSIFIED] New security protocols archived\n"
             print("\nSECURE PRESERVATION:")
-            file.write("\n" + log)
-            print(log)
+            protocols.write(log)
+            print(log, end="")
     except FileNotFoundError as e:
         print(f"Error classified data: '{e.filename}' not found")
     except Exception as e:
         print(f"Error: {e}")
-    print("Vault automatically sealed upon completion\n")
-    print("All vault operations completed with maximum security.")
+    if protocols and protocols.closed and classified and classified.closed:
+        print("Vault automatically sealed upon completion\n")
+        print("All vault operations completed with maximum security.")
+    elif protocols and classified:
+        print("Error: critical error, vault not closed")
 
 
 if __name__ == "__main__":
