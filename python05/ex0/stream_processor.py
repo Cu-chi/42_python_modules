5966fbd019d4db2a246931934a30b5db5ddfd0ec
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-02-10 09:38:05 +0100

    dev(module05/ex0): not abstract

--- python05/ex0/stream_processor.py
+++ python05/ex0/stream_processor.py
@@ -11,7 +11,6 @@ class DataProcessor(ABC):
     def validate(self, data: Any) -> bool:
         pass
 
-    @abstractmethod
     def format_output(self, result: str) -> str:
         return "Output: " + result
 
