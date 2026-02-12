e35f20f6aec466f33971454a9510e193e537c6f9
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-02-12 18:13:40 +0100

    dev(module07/ex0): work in progress

---
+++ python07/ex0/Card.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from enum import Enum
+
+
+class CardRarity(Enum):
+    COMMON = "Common"
+    UNCOMMON = "Uncommon"
+    RARE = "Rare"
+    LEGENDARY = "Legendary"
+
+
+class Card(ABC):
+    def __init__(self, name: str, cost: int, rarity: str) -> None:
+        self.name: str = name
+        self.cost: int = cost
+        self.rarity: str = ""
+        for card_rarity in CardRarity:
+            if rarity == card_rarity.value:
+                self.rarity: str = rarity
+        if self.rarity == "":
+            raise ValueError(f"rarity '{rarity}' is invalid")
+
+    @abstractmethod
+    def play(self, game_state: dict) -> dict:
+        pass
+
+    def get_card_info(self) -> dict:
+        return {
+            "name": self.name,
+            "cost": self.cost,
+            "rarity": self.rarity,
+        }
+
+    def is_playable(self, available_mana: int) -> bool:
+        return available_mana >= self.cost
---
+++ python07/ex0/CreatureCard.py
@@ -0,0 +1,28 @@
+from .Card import Card
+
+
+class CreatureCard(Card):
+    def __init__(self, name: str, cost: int, rarity: str,
+                 attack: int, health: int) -> None:
+        super().__init__(name, cost, rarity)
+        if attack > 0:
+            self.attack: int = attack
+        else:
+            raise ValueError("'attack' attribute must be positive")
+        if health > 0:
+            self.health: int = health
+        else:
+            raise ValueError("'health' attribute must be positive")
+
+    def get_card_info(self) -> dict:
+        infos: dict = super().get_card_info()
+        infos.update({
+            "attack": self.attack,
+            "health": self.health
+        })
+
+    def play(self, game_state: dict) -> dict:
+        pass
+
+    def attack_target(self, target) -> dict:
+        pass
