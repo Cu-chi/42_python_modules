99cdd6f18fd51d022cfdc36192f7a16c3c1ec7e5
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-02-22 10:02:12 +0100

    fix(module02/ex0): typo

--- python02/ex0/ft_first_exception.py
+++ python02/ex0/ft_first_exception.py
@@ -27,7 +27,7 @@ def test_temperature_input() -> None:
     print("Testing temperature: -50")
     check_temperature("-50")
     print()
-    print("All tests completed - progran didn't crash!")
+    print("All tests completed - program didn't crash!")
 
 
 if __name__ == "__main__":
